7fa63ba5b5c2087eae6bfbd746f7f21a7f88ea59
lelana0824 <lelana0824@gmail.com>
2026-02-24 14:54:31 +0900

    버블 정렬 함수 구현 및 내용 진행
        - 버블 정렬 함수 구현
        - 함수 포인터로 비교 로직 전달하도록 구현

---
+++ BubbleSort/Main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+// 함수 포인터 타입 지정
+
+using Comparer = bool (*)(int, int);
+
+// 함수 포인터 활용
+bool IsGreater(int left, int right)
+{
+	return left > right;
+}
+
+bool IsLess(int left, int right)
+{
+	return left < right;
+}
+
+// 버블 정렬 함수
+// 비교 조건 확인 로직을 위임. 델리게이트
+// c++ 일반 함수 - Function
+// 객체의 멤버 함수 - Method
+void BubbleSort(int* array, int length, Comparer comparer)
+{
+	for (int i = 0; i < length - 1; i++)
+	{
+		for (int j = 0; j < length - 1 - i; j++)
+		{
+			// 비교
+			if (comparer(array[j], array[j + 1]))
+			{
+				// 교체
+				std::swap<int>(array[j], array[j + 1]);
+			}
+		}
+	}
+}
+
+// 배열 항목 출력
+void PrintArray(int* array, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		std::cout << array[i];
+		if (i < length - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	std::cout << "\n";
+}
+
+int main()
+{
+	// 자료 집합.
+	int array[] = { 5, 2, 8, 4, 1, 7, 3, 6, 9, 10, 15, 13, 14, 12, 17, 16 };
+
+	int length = sizeof(array) / sizeof(array[0]);
+
+	std::cout << "정렬 전 배열: ";
+	PrintArray(array, length);
+
+	BubbleSort(array, length, IsLess);
+
+	std::cout << "정렬 후 배열: ";
+	PrintArray(array, length);
+	std::cin.get();
+}

9ef116ea5c09ec851603396e8e22072bd2b3f66f
lelana0824 <lelana0824@gmail.com>
2026-02-24 16:50:58 +0900

    삽입 정렬 구현
    - 삽입 정렬 함수 및 예제 구현

---
+++ InsertionSort/Main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <Windows.h>
+
+// 삽입 정렬 구현
+void InsertionSort(int* array, int length)
+{
+	if (length <= 1) return;
+
+	
+	// 배열 순회
+	for (int i = 1; i < length; i++)
+	{
+		// 현재 삽입할 값.
+		int key = array[i];
+		int j = i - 1;
+
+		// key보다 큰 값은 오른쪽으로 배치.
+		while (j >= 0 && array[j] > key)
+		{
+			array[j + 1] = array[j];
+			--j;
+		}
+
+		// 적절한 위치에 키 삽입.
+		array[j + 1] = key;
+
+	}
+}
+
+
+
+// 배열 항목 출력
+void PrintArray(int* array, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		std::cout << array[i];
+		if (i < length - 1)
+		{
+			std::cout << ", ";
+		}
+	}
+
+	std::cout << "\n";
+}
+
+
+
+int main()
+{
+	// 자료 집합.
+	int array[] = { 5, 2, 8, 4, 1, 7, 3, 6, 9, 10, 15, 13, 14, 12, 17, 16 };
+	int length = ARRAYSIZE(array);
+
+	std::cout << "정렬 전 배열: ";
+	PrintArray(array, length);
+
+	InsertionSort(array, length);
+
+	std::cout << "정렬 후 배열: ";
+	PrintArray(array, length);
+
+	std::cin.get();
+}
